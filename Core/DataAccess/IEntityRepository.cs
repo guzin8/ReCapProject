39f3bb4147ab56b6afa923f07a5fef6dc6533b73
guzin8 <kanburogluguzin@gmail.com>
2021-02-14 19:11:57 -0800

    WebAPI eklendi.

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -8,7 +8,7 @@ namespace Core.DataAccess
 {
    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
-        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        List<T> GetAll(Expression<Func<T, bool>> filter=null);
         T GetById(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
